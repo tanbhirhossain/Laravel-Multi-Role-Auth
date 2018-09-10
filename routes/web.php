f6bf1dbe2b3efd5c85a93b6d2c135e12faac9f50
Tanbhir Hossain <r25n.cstanbhir@gmail.com>
2018-09-10 23:22:35 +0600

    LARAVEL MULTI ROLE AUTH

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin', function(){
+    echo "Hello Admin";
+})->middleware('auth','admin');
+
+Route::get('/agent', function(){
+    echo "Hello Agent";
+})->middleware('auth','agent');
+
+Route::get('/customer', function(){
+    echo "Hello Customer";
+})->middleware('auth','customer');
